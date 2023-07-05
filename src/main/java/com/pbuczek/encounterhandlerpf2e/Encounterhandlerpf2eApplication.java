27063250e2a19816f375a9af4bf5df92d936ab87
Pawel <pawel.buczek@email.com>
2023-07-05 16:43:21 +0200

    Initial commit

---
+++ src/main/java/com/pbuczek/encounterhandlerpf2e/Encounterhandlerpf2eApplication.java
@@ -0,0 +1,13 @@
+package com.pbuczek.encounterhandlerpf2e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Encounterhandlerpf2eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Encounterhandlerpf2eApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbuczek/encounterhandlerpf2e/Encounterhandlerpf2eApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbuczek.encounterhandlerpf2e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Encounterhandlerpf2eApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
