afaa30c0cff979c1936025d26e32ad604fe0df43
Pawel <pawel.buczek@email.com>
2023-07-09 11:16:41 +0200

    improved formatting

--- src/main/java/com/pbuczek/pf/security/controller/UserController.java
+++ src/main/java/com/pbuczek/pf/security/controller/UserController.java
@@ -2,7 +2,6 @@ package com.pbuczek.pf.security.controller;
 
 import com.pbuczek.pf.security.User;
 import com.pbuczek.pf.security.UserType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,7 @@ public class UserController {
     @GetMapping(value = "/{userId}")
     public User getUser(@PathVariable Integer userId) {
 
-        return new User(userId, UserType.STANDARD,"test", "test.mail@com",null,null);
+        return new User(userId, UserType.STANDARD, "test", "test.mail@com", null, null);
     }
 
 }
