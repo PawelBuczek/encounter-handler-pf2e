aabda006935987de8ddd48e63a9852000a3c3978
Pawel <pawel.buczek@email.com>
2023-07-10 15:02:21 +0200

    removed unnecessary data in the API call to create user

--- src/main/java/com/pbuczek/pf/security/User.java
+++ src/main/java/com/pbuczek/pf/security/User.java
@@ -1,27 +1,32 @@
 package com.pbuczek.pf.security;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "user")
 public class User {
 
+    public User(UserType type, String username, String email) {
+        this.type = type;
+        this.username = username;
+        this.email = email;
+    }
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Enumerated(EnumType.STRING)
-    private UserType type;
+    private UserType type = UserType.STANDARD;
     @Enumerated(EnumType.STRING)  // doesn't have any meaning for ADMIN user type
-    private PaymentPlan paymentPlan;
+    private PaymentPlan paymentPlan = PaymentPlan.FREE;
 
     private String username;
     private String email;
     @Column(columnDefinition = "BINARY")
-    private byte[] password;
+    private byte[] password = new byte[0];
 }
