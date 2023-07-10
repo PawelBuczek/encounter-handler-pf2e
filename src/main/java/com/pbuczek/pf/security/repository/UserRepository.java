71196fef21458a5f325be346f6d388f7aabb4ba7
Pawel <pawel.buczek@email.com>
2023-07-10 14:03:50 +0200

    added finding by username and removed unnecessary method

--- src/main/java/com/pbuczek/pf/security/repository/UserRepository.java
+++ src/main/java/com/pbuczek/pf/security/repository/UserRepository.java
@@ -6,15 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    List<User> findAllById(Integer id);
 
     Optional<User> findById(Integer id);
 
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String email);
 
     @Transactional
     @Modifying
