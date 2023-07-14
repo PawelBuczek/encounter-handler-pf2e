61489800c5bc74cea5388214ca2e25f3a055de8a
Pawel <pawel.buczek@email.com>
2023-07-14 13:43:32 +0200

    small correction of commentary for docker-images-setup.sh

--- docker-images-setup.sh
+++ docker-images-setup.sh
@@ -4,7 +4,6 @@
 # Please note that it will try to remove previous container, which can lead for example to removal of database data.
 # That is not a problem on local setup because we are using Liquibase (and db will be created on application startup).
 # But additional data created by you locally will be lost.
-# On Production environment due to a different setup running this script will not do anything.
 
 # Standard output that is being echoed at the end is the docker container id where mysql image is now running
 
