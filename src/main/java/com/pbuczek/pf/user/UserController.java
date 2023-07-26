16dc49d3aa7f0a5f9dbd57aa87e9dc8f85f8dabf
PawelBuczek <pawel.buczek@email.com>
2023-07-26 18:30:08 +0200

    method name update

--- src/main/java/com/pbuczek/pf/user/UserController.java
+++ src/main/java/com/pbuczek/pf/user/UserController.java
@@ -203,7 +203,7 @@ public class UserController {
 
     @PatchMapping(path = "/lock-unlock/{userId}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public User lockUnlockUser(@PathVariable Integer userId) {
+    public User lockUnlock(@PathVariable Integer userId) {
         User user = userRepo.findById(userId).orElseThrow(() ->
                 new ResponseStatusException(HttpStatus.NOT_FOUND,
                         String.format("user with id '%d' not found", userId)));
