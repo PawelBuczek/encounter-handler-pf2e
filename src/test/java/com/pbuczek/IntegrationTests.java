3a6a1feee90b9da640e0b75022185c81c284b17a
PawelBuczek <pawel.buczek@email.com>
2023-07-27 03:50:36 +0200

    added IntegrationTests suite

--- src/test/java/com/pbuczek/IntegrationTests.java
+++ src/test/java/com/pbuczek/IntegrationTests.java
@@ -1,15 +1,15 @@
-package com.pbuczek.pf;
+package com.pbuczek;
 
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
-@SelectPackages("com.pbuczek.pf")
+@SelectPackages("com.pbuczek.pf.it")
 
-@IncludeTags("UnitTest")
+@IncludeTags("IntegrationTest")
 @Suite
-@SuiteDisplayName("UnitTests")
-public class UnitTests {
+@SuiteDisplayName("IntegrationTests")
+public class IntegrationTests {
 
 }
---
+++ src/test/java/com/pbuczek/pf/it/UserIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.pbuczek.pf.it;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("IntegrationTest")
+class UserIntegrationTest {
+
+    @Test
+    void userTest() {
+
+    }
+}
