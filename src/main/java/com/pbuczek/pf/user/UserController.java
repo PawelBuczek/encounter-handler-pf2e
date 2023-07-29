f05b5daf08983d175434c2b9d74ec69389c51989
PawelBuczek <pawel.buczek@email.com>
2023-07-29 09:58:37 +0200

    playing around with regex and file reading

--- src/main/java/com/pbuczek/pf/user/UserController.java
+++ src/main/java/com/pbuczek/pf/user/UserController.java
@@ -11,6 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 @RestController
@@ -19,7 +22,7 @@ public class UserController {
 
     private final static String PASSWORD_REGEX = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9]).{8,50}$";
     // below regex constant needs to match with sql rule created in the file 'src/main/resources/db/sql-files/add-user-email-validation-constraint.sql'
-    private final static String EMAIL_REGEX = "^[a-zA-Z0-9][a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]*?[a-zA-Z0-9._-]?@[a-zA-Z0-9][a-zA-Z0-9._-]*?[a-zA-Z0-9]?\\.[a-zA-Z]{2,63}$";
+    private static String EMAIL_REGEX;
 
     private final PasswordEncoder passwordEncoder = SecurityHelper.passwordEncoder;
 
@@ -27,9 +30,10 @@ public class UserController {
     private final SecurityHelper securityHelper;
 
     @Autowired
-    public UserController(UserRepository userRepo, SecurityHelper securityHelper) {
+    public UserController(UserRepository userRepo, SecurityHelper securityHelper) throws IOException {
         this.userRepo = userRepo;
         this.securityHelper = securityHelper;
+        EMAIL_REGEX = Files.readString(Path.of("com/pbuczek/pf/user/emailRegex"));
     }
 
     @PostMapping
