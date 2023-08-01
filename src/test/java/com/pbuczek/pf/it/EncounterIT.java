668031037ce524c66010e36ab820ecda4cf0e120
PawelBuczek <pawel.buczek@email.com>
2023-08-01 08:53:02 +0200

    first tries with MockMvc

--- src/test/java/com/pbuczek/pf/it/EncounterIT.java
+++ src/test/java/com/pbuczek/pf/it/EncounterIT.java
@@ -1,5 +1,9 @@
 package com.pbuczek.pf.it;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.pbuczek.pf.TestUserDetails;
 import com.pbuczek.pf.encounter.Encounter;
 import com.pbuczek.pf.encounter.EncounterDto;
@@ -9,18 +13,18 @@ import com.pbuczek.pf.user.UserDto;
 import com.pbuczek.pf.user.UserRepository;
 import com.pbuczek.pf.user.UserType;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -29,24 +33,43 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Tag("IntegrationTest")
 @AutoConfigureObservability
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class EncounterIT implements TestUserDetails {
 
     private final List<Integer> createdUserIds = new ArrayList<>();
     private final List<Integer> createdEncounterIds = new ArrayList<>();
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static ObjectWriter ow;
 
     @Autowired
-    TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Autowired
-    UserRepository userRepo;
+    private TestRestTemplate restTemplate;
+
     @Autowired
-    EncounterRepository encounterRepo;
+    private UserRepository userRepo;
+    @Autowired
+    private EncounterRepository encounterRepo;
+
+    @BeforeAll
+    static void initialize() {
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        mapper.registerModule(new JavaTimeModule());
+        ow = mapper.writer().withDefaultPrettyPrinter();
+    }
 
     @BeforeEach
     void setUp() {
@@ -68,7 +91,7 @@ class EncounterIT implements TestUserDetails {
     }
 
     @Test
-    void encounterIsCreatedCorrectly() {
+    void encounterIsCreatedCorrectly() throws Exception {
         int userId = createUserAndGetId(TEST_USERNAME_2, TEST_EMAIL_2);
         ResponseEntity<Encounter> response = getResponseForCreatingEncounter("test", userId, "test");
 
@@ -80,6 +103,17 @@ class EncounterIT implements TestUserDetails {
         assert createdEncounter != null;
         assertThat(createdEncounter.getId()).isNotNull();
         createdEncounterIds.add(createdEncounter.getId());
+
+        MvcResult result = this.mockMvc.perform(
+                        post("/encounter")
+                                .header("Authorization", getBasicAuthenticationHeader(TEST_USERNAME_1, TEST_PASSWORD))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(ow.writeValueAsString(new EncounterDto("mockmvc", userId, "mockmvc"))))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("mockmvc")))
+                .andReturn();
+        Encounter enc = mapper.readValue(result.getResponse().getContentAsString(), Encounter.class);
+        createdEncounterIds.add(enc.getId());
 
         assertAll("Verify createdEncounter properties",
                 () -> assertThat(createdEncounter.getName()).isEqualTo("test"),
