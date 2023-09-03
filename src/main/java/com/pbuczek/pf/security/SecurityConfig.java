05f6dabee8a1d0a187932301fd5c1c2a1b3da1c3
PawelBuczek <pawel.buczek@email.com>
2023-09-03 13:16:52 +0200

    added oauth2 jwt to SecurityFilterChain

--- src/main/java/com/pbuczek/pf/security/SecurityConfig.java
+++ src/main/java/com/pbuczek/pf/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -44,6 +45,7 @@ public class SecurityConfig {
                 .httpBasic(withDefaults())
                 // this is NOT a filter!
                 .addFilterBefore(new ApiKeyAuthenticationAdder(userDetailsService), UsernamePasswordAuthenticationFilter.class)
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
 }
