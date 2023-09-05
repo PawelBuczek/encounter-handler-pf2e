62b37cda9ff2e1d2eaeb5f75c3e01f29b2f4497b
PawelBuczek <pawel.buczek@email.com>
2023-09-05 15:01:06 +0200

    optimized imports

--- src/main/java/com/pbuczek/pf/security/SecurityConfig.java
+++ src/main/java/com/pbuczek/pf/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.pbuczek.pf.security;
 
-import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
@@ -22,7 +21,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
